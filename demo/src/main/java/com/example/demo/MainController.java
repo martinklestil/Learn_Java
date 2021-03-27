3220109c7ee2e9fd791d6f03fd500fbcb64d350c
Martin Klestil <34619286+martinklestil@users.noreply.github.com>
2021-03-27 20:27:13 +0100

    work with thymeleaf

--- demo/src/main/java/com/example/demo/MainController.java
+++ demo/src/main/java/com/example/demo/MainController.java
@@ -2,16 +2,19 @@ package com.example.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MainController {
 	
 	@GetMapping("/index")
-	String index() {
+	ModelAndView index() {
 		
 		// Business Logik
+		ModelAndView modelAndView = new ModelAndView("index");
+		modelAndView.addObject("title", "Zeiteinträge Anwendung");
 		
-		return "index";
+		return modelAndView;
 	}
 
 }
