b3ffdb9f4deec77e021b144d58acf0f281bc637a
Martin Klestil <34619286+martinklestil@users.noreply.github.com>
2021-04-03 21:00:22 +0200

    Update TimeEntriesRepository.java

--- Time_Spring/src/main/java/com/example/demo/TimeEntriesRepository.java
+++ Time_Spring/src/main/java/com/example/demo/TimeEntriesRepository.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.springframework.data.repository.CrudRepository;
 
+//Datenbank
 public interface TimeEntriesRepository extends CrudRepository<TimeEntry, Long> {
 
 }
